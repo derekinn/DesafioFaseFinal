4d2d9c43bbab137e817e363c400c3d3d7e25a89a
Derek <38166940+derekinn@users.noreply.github.com>
2026-09-03 08:19:14 -0300

    Refatoração em partes

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5026748f444525ed0f9d8ef465a1adc064e3b0b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+292f267723941edcf41ec144a8998f00375f03d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
