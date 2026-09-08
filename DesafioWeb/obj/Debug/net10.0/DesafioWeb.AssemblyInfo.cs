9b7ee040d690bdc426ebf9919e1f1314b3261fdc
Derek <38166940+derekinn@users.noreply.github.com>
2026-09-08 16:30:54 -0300

    Att

--- DesafioWeb/obj/Debug/net10.0/DesafioWeb.AssemblyInfo.cs
+++ DesafioWeb/obj/Debug/net10.0/DesafioWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be7a96b4b332dd2d280e102245e652827bdc14be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b4107298fd467f2dd27077d051586cb4a7b545")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
